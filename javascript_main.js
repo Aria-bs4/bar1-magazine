b0fc8c250cfb6cf1f3627fe57dda5a20bfe3a079
aria_m <aria_bs4@yahoo.com>
2020-04-27 01:57:02 +0300

    finish it using bootsrap , font awesome , jquery , swiper slider. its not response yet and sliders dont have seperate function and seperate features

---
+++ javascript_main.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+
+$(document).ready(function(){
+    
+});
